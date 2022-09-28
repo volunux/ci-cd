0d0a26954c56f9a73d39b992b95ce032fd932252
volunux <volunux@gmail.com>
2022-09-28 10:48:20 +0100

    ci-cd project

---
+++ src/test/java/com/volunux/cicd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunux.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
